0dc24cc9b1d2bc36b3e1f364ceb5f6cbd8cb759e
Anderson Aka <andersonmanouan@gmail.com>
2024-10-29 20:07:28 +0000

    update qr-code.tsx

--- components/adhesion/qr-code.tsx
+++ components/adhesion/qr-code.tsx
@@ -5,7 +5,7 @@ const QrCodeMobileMoney: React.FC = () => {
     const [text, setText] = React.useState('07 07 88 94 49');
 
     return (
-        <div className='flex flex-row space-x-5'>
+        <div className='flex flex-wrap space-x-5'>
             <Space direction="vertical" align="center">
                 <label className="block text-lg font-medium text-dark"> Orange Money</label>
                 <QRCode value={text || '-'} />
