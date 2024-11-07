cf7a8661b7a8ad179d48990dc2d59fcedfc1d659
Anderson Aka <andersonmanouan@gmail.com>
2024-11-07 21:37:12 +0000

    build(deps): update radio-payer-maintenant-plustard-cpt.tsx, package-lock.json and package.json

--- components/communs/radio-payer-maintenant-plustard-cpt.tsx
+++ components/communs/radio-payer-maintenant-plustard-cpt.tsx
@@ -1,5 +1,6 @@
 import { Flex, Radio, DatePicker, DatePickerProps } from 'antd'
-import React from 'react'
+import React, { useState } from 'react'
+import dayjs from 'dayjs';
 
 interface Props {
     handleRadioChange: (value: string) => void
@@ -8,8 +9,15 @@ interface Props {
     choix: string
     modePaiement: string
 }
-
+// const dateFormat = 'YYYY-MM-DD';
 const RadioGroupPayerMaintenantPlusTard = ({ handleRadioChange, handleRadioChange2, onChangeDate, choix, modePaiement }: Props) => {
+    // const [date, setDate] = useState<dayjs.Dayjs | null>(null); // Valeur initiale modifiée
+    // const formattedDateDebut = date?.format('YYYY-MM-DD');
+    // Définir la plage de date min et max pour la sélection
+    const minDate = dayjs().subtract(0, 'day'); // Aujourd'hui -1 jour
+    const disabledDate = (current: dayjs.Dayjs) => {
+        return current && (current < minDate); // Désactiver les dates en dehors de l'intervalle
+    };
     return (
         <>
             <Flex vertical gap="middle">
@@ -47,7 +55,7 @@ const RadioGroupPayerMaintenantPlusTard = ({ handleRadioChange, handleRadioChang
                                 <span className="block">Payer plus tard</span>
                                 {choix === '2' ? (
                                     <div className='w-'>
-                                        <DatePicker onChange={onChangeDate} placeholder='Choisir une date' style={{ width: 150 }} />
+                                        <DatePicker disabledDate={disabledDate} onChange={onChangeDate} placeholder='Choisir une date' style={{ width: 150 }} />
                                         {/* {error && <p style={{ color: 'red', fontSize: '0.875rem' }}>{error}</p>} */}
                                     </div>
                                 ) : null}
